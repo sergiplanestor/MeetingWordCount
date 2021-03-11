8d2464bd7cafce4c939e4198f9f9650e318d5b9d
Sergi <joseprf@ext.bershka.com>
2021-03-11 19:23:21 +0100

    Base viewmodel created

---
+++ presentation/src/main/java/com/revolhope/presentation/library/base/BaseViewModel.kt
@@ -0,0 +1,20 @@
+package com.revolhope.presentation.library.base
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+abstract class BaseViewModel : ViewModel(), CoroutineScope {
+
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+}
