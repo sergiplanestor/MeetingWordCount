3b51d389ffc40edb5537c02f61032f0a7500555d
Sergi <joseprf@ext.bershka.com>
2021-03-12 20:13:20 +0100

    Updated word model. Removed useless param

--- domain/src/main/java/com/revolhope/domain/feature/word/model/WordModel.kt
+++ domain/src/main/java/com/revolhope/domain/feature/word/model/WordModel.kt
@@ -1,8 +1,10 @@
 package com.revolhope.domain.feature.word.model
 
+import java.util.UUID
+
 data class WordModel(
+    val id: String = UUID.randomUUID().toString(),
     val word: String,
     val occurrences: Int,
-    val originalFileName: String,
-    val occurrencesWords: List<String>
+    val originalFileName: String
 )
